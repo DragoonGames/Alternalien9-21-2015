bb95f979f1b88400fbf4461daac0cccd618bb6e2
peck2703 <mrpeckjr@gmail.com>
2015-09-28 20:32:06 -0500

    Real Tutorial is Back and Devon's Jungle Level is Missing Prefab

---
+++ Assets/Scripts/TextBoxVisibility.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBoxVisibility : MonoBehaviour {
+
+    public GameObject OnTextBox;
+    public GameObject OffTextBox;     //Turn off previous text Overlays
+
+    // Use this for initialization
+    void Start()
+    {
+        OnTextBox.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        OnTextBox.SetActive(true);
+        OffTextBox.SetActive(false);
+        print("woo");
+    }
+}
---
+++ Assets/Scripts/TextVisibility.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class Visibiliy : MonoBehaviour 
+{
+    public GameObject TextBox1;
+    public Collider2D collider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        TextBox1.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D chocolate)
+    {
+        chocolate.gameObject.SendMessage("ShowText");
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        TextBox1.SetActive(true);
+    }
+    void ShowText()
+    {
+        TextBox1.SetActive(true);
+    }
+
+}
