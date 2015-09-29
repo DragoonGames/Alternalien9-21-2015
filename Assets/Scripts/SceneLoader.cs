33a19415d6bfdd1b2cf6c578ce79205a3cc7bddb
peck2703 <mrpeckjr@gmail.com>
2015-09-28 21:57:12 -0500

    Fireballs Added

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+    public string pastLevel;
+    public string nextLevel;
+    public string mainMenu;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Application.LoadLevel(nextLevel);
+    }
+}
