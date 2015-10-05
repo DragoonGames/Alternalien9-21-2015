2bf95b0553ef950c43f7845d12bf2cd4d1c2c422
peck2703 <harmsene@MM172-900373D7.ad.matc.edu>
2015-10-05 11:26:05 -0500

    Main Menu and GUI Updates

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,12 +2,57 @@
 using System.Collections;
 
 public class SceneLoader : MonoBehaviour {
-    public string pastLevel;
+    public string levelSelect;
     public string nextLevel;
     public string mainMenu;
-
-    void OnTriggerEnter2D(Collider2D other)
+	public Camera mainCamera;
+	private bool win = true;
+    /*void OnTriggerEnter2D(Collider2D other)
     {
         Application.LoadLevel(nextLevel);
     }
+    */
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		win = true;
+	}
+	
+	void OnGUI()
+	{
+		print ("Render");
+		/*if (win) {	
+			// Make a background box
+			GUI.Box (new Rect (420, 100, 125, 120), "Loader Menu");
+			
+			// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+			if (GUI.Button (new Rect (430, 130, 100, 20), "Next Level")) {
+				Application.LoadLevel (nextLevel);
+			}
+			
+			// Make the second button.
+			if (GUI.Button (new Rect (430, 155, 100, 20), "Level Select")) {
+				Application.LoadLevel (levelSelect);
+			}
+			
+			// Make the third button.
+			if (GUI.Button (new Rect (430, 180, 100, 20), "Exit")) {
+				Application.LoadLevel (mainMenu);
+			}
+		}
+		*/
+
+		if (win) {
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100)/2, (mainCamera.pixelHeight - 120)/2, 100, 20), "Next Level")){
+				Application.LoadLevel(nextLevel);
+			}
+			if (GUI.Button (new Rect((mainCamera.pixelWidth - 100)/2, (mainCamera.pixelHeight - 60)/2, 100, 20), "Level Select")){
+				Application.LoadLevel(levelSelect);
+			}
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100)/2, (mainCamera.pixelHeight - 0)/2, 100, 20), "Main Menu")){
+				Application.LoadLevel(mainMenu);
+			}
+			
+		}
+	}
 }
---
+++ Assets/mainGUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainGUI : MonoBehaviour {
+
+	public string levelSelect;
+	public string nextLevel;
+	public string mainMenu;
+	public Camera mainCamera;
+	public bool optionButtonPressed = false;
+	// Use this for initialization
+	void OnGUI()
+	{
+		if (!optionButtonPressed) {
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 120) / 2, 100, 20), "Next Level")) {
+				Application.LoadLevel (nextLevel);
+			}
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 60) / 2, 100, 20), "Level Select")) {
+				Application.LoadLevel (levelSelect);
+			}
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 0) / 2, 100, 20), "Options")) {
+				optionButtonPressed = true;
+			}
+		} else if (optionButtonPressed) {
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 180) / 2, 100, 20), "Video Settings")) {
+				Application.LoadLevel (nextLevel);
+			}
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 120) / 2, 100, 20), "Audio Settings")) {
+				Application.LoadLevel (levelSelect);
+			}
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 60) / 2, 100, 20), "Character Bios")) {
+				//Application.LoadLevel(mainMenu);
+			}
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 0) / 2, 100, 20), "Back")) {
+				optionButtonPressed = false;
+			}
+			//PlayerPrefs.SetInt("LevelUnlocked")
+		}
+
+	}
+}
