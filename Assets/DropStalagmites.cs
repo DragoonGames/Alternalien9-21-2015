f2cc64d9e1e8f6df6b3ecd7ff6bc15c9e99a06f1
peck2703 <mrpeckjr@gmail.com>
2015-10-17 01:00:14 -0500

    Drop Stalagmites

---
+++ Assets/DropStalagmites.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropStalagmites : MonoBehaviour {
+
+    public GameObject objectToDrop;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D objectRigidbody = objectToDrop.AddComponent<Rigidbody2D>();
+        objectRigidbody.mass = 5;
+        objectRigidbody.gravityScale = 5;
+    }
+}
