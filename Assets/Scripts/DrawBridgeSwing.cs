cda61e5fa8ecb5ff0635ced7362fc2d9e4835edd
punkasaurus <erincfink@gmail.com>
2015-11-02 09:09:27 -0600

    Swing
    Swinging vines and draw bridge swing. Draw bridge needs to be tweaked.
    Need a sprite with the pivot all the way to the top for vine or left (or
    right) for draw bridge.

---
+++ Assets/Scripts/DrawBridgeSwing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawBridgeSwing : MonoBehaviour {
+	public float DbRotation;
+	public bool swingUp = true;
+	
+	void Update(){
+		DbRotation = Mathf.Clamp (DbRotation, 1, 45);
+		if (Input.GetKeyDown (KeyCode.U)) {
+			swingUp = true;
+			if (swingUp){
+				DbRotation += .5f;
+				transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, DbRotation);
+				if(DbRotation >= 45)
+					swingUp = false;
+			} 
+		}
+		else if (Input.GetKeyDown(KeyCode.J)){
+			swingUp = true;
+			if (swingUp){
+				DbRotation += .5f;
+				transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, DbRotation);
+				if(DbRotation >= 45)
+					swingUp = true;
+			} 
+		}
+	}
+}
---
+++ Assets/Scripts/SwingVine.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingVine : MonoBehaviour {
+	public float zRotation;
+	public bool swingRight = true;
+	
+	void Update(){
+		zRotation = Mathf.Clamp (zRotation, 225, 315);
+		if (swingRight) {
+			zRotation += .5f;
+			transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, zRotation);
+			if(zRotation >= 315)
+				swingRight = false;
+		} 
+		else{
+			zRotation -= .5f;
+			transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, zRotation);
+			if (zRotation <= 225)
+				swingRight = true;
+		}
+	}
+}
