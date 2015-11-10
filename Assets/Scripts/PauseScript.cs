8b1298a2a97163949dbc6fd6d6c79b8370f2534f
peck2703 <harmsene@MM172-900373D7.ad.matc.edu>
2015-11-10 11:51:53 -0600

    Pause Script with other Meta files

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+	public bool gamePaused = false;
+
+	public float pauseBoxWidth = 250;
+	public float pauseBoxHeight = 150;
+
+	public float resumeButtonWidth = 250;
+	public float resumeButtonHeight = 200;
+	public float resumeButtonWidthOffset = 100;
+	public float resumeButtonHeightOffset = 100;
+
+	public float quitButtonWidth = 250;
+	public float quitButtonHeight = 200;
+	public float quitButtonWidthOffset = 100;
+	public float quitButtonHeightOffset = 100;
+
+	public Texture pauseTexture;
+	public Texture resumeButtonTexture;
+	public Texture quitButton;
+
+	public GUIStyle myStyle;
+
+	void OnGUI()
+	{
+		if (gamePaused) {
+			GUI.Box(new Rect((Screen.width / 2) - (pauseBoxWidth / 2),
+			                 (Screen.height / 2) - (pauseBoxHeight / 2),
+			                 pauseBoxWidth, pauseBoxHeight), pauseTexture, myStyle);
+
+			if(GUI.Button(new Rect(((Screen.width / 2) - (resumeButtonWidth / 2))-resumeButtonWidthOffset,
+			                       ((Screen.height / 2) - (resumeButtonHeight / 2)) - resumeButtonHeightOffset,
+			                       resumeButtonWidth, resumeButtonHeight),"Resume"))
+			{
+				Time.timeScale = 1;
+			}
+			if (GUI.Button(new Rect(((Screen.width / 2) - (quitButtonWidth / 2)) - quitButtonWidthOffset,
+			                        ((Screen.height / 2) - (quitButtonHeight / 2)) - quitButtonHeightOffset,
+			                        resumeButtonWidth, resumeButtonHeight),"Quit"))
+				Application.LoadLevel(0);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
