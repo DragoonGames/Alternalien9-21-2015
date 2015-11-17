8fae22684667cc620946c9f1c3f9d3fbaa990bef
peck2703 <mrpeckjr@gmail.com>
2015-11-17 11:31:08 -0600

    Adding Scripts

---
+++ Assets/Scripts/AddAlien.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddAlien : MonoBehaviour {
+
+    public GameObject UnlockObject;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UnlockObject.gameObject == null)
+        {
+            Camera.main.SendMessage("UnlockAliens");
+        }
+    }
+}
