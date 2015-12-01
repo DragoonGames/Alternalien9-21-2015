ab4b8698ba15abd37e9a364e83bf528a2c228958
djjterminator <djjbionics@yahoo.com>
2015-12-01 11:46:33 -0600

    Finish Desert Level
    Things still needed to be done:
    - Lenny needs to move Clone
    - Clone needs to change Tag during power
    - Adjust jump for cacti jump
    - checkout Drawbrdige
    - Quicksand script?? (change gravity scale to high number)
    - Instantiate new crate in case crate destroyed
    - LOCK ALIEN IN CAGE IN CONTROL SCRIPT
    - Adjust jump for cacti jump

--- Assets/DesertDrawBridge.cs
+++ Assets/DesertDrawBridge.cs
@@ -1,32 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class DrawBridgeSwing : MonoBehaviour {
+public class DesertDrawBridge : MonoBehaviour {
+
 	public float DbRotation = 85.5f;
-    public bool swingDown = false;
+	public bool swingDown;
+	public GameObject[] accessCards;
+	public int count;
 
 	void Start(){
 		DbRotation -= 89.5f;
 		transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, DbRotation);
+		accessCards = GameObject.FindGameObjectsWithTag ("isCardKey");
+		count = 0;
+		swingDown = false;
 	}
 	
 	void Update(){
 		DbRotation = Mathf.Clamp (DbRotation, 1, 89.5f);
 
+		for (int i = 0; i < accessCards.Length; i++) {
+
+			if (accessCards [i] = null) {
+				count++;
+			}
+			if (count == accessCards.Length) {
+				swingDown = true;
+			}
+		}
 		if (swingDown){
 			DbRotation += .5f;
 			transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, DbRotation);
-			if(DbRotation >= 89.5 && Input.GetKey(KeyCode.U))
-				swingDown = false;
-		} 
-
-		else if(!swingDown){
-			DbRotation -= .5f;
-			transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, DbRotation);
-			if(DbRotation <= 1 && Input.GetKey(KeyCode.J))
-				swingDown = true;
 		} 
 	}
-
-
 }
